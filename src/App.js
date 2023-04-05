a7d6ba44e3a53002dfdc22611d601f84f5948011
Neidn <dty3152@gmail.com>
2023-04-05 15:55:00 +0900

    use react router and add error page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
+import ErrorPage from "./pages/Error";
 import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage/>,
+    element: <RootLayout/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: "/",
+        element: <HomePage/>,
+      },
+      {
+        path: "/products",
+        element: <ProductsPage/>,
+      },
+    ],
   },
 ]);
 
-const App = function () {
+const App = () => {
   return <RouterProvider router={router}/>;
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
-function HomePage() {
+import {Link} from "react-router-dom";
+
+const HomePage = () => {
   return (
-      <h1>My Home Page</h1>
+      <>
+        <h1>My Home Page</h1>
+        <p>Go to <Link to="/products">the list of Products</Link>.</p>
+      </>
   );
-}
+};
 
 export default HomePage;
